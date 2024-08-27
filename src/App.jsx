2eaac28bbcf769488c3c256cb265dbfdd9ee73f6
marlisapee <mpee@tripadvisor.com>
2024-08-27 14:55:58 -0400

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import LandingView from './views/LandingView';
+
 import './App.css';
-import useAllBooks from './hooks/useAllBooks';
-import useSingleBook from './hooks/useSingleBook';
 
 function App() {
-  const { books, loading, error } = useAllBooks();
-  const {
-    book: currentBook,
-    loading: bookLoading,
-    error: bookError,
-  } = useSingleBook(1);
-
-  if (loading) return <p>Loading...</p>;
-
   return (
     <>
-      {books &&
-        books.length > 0 &&
-        books.map((book) => (
-          <div key={book.id}>
-            <p>{book.title}</p>
-
-            {book.authors.map((author) => (
-              <p key={author.id}>{author.name}</p>
-            ))}
-          </div>
-        ))}
-      {currentBook && (
-        <>
-          <p>current book: </p>
-          <p>{currentBook.title}</p>
-        </>
-      )}
+      <Routes>
+        <Route path="/" element={<LandingView />} />
+      </Routes>
     </>
   );
 }
