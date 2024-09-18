2285f90a6f7ca8b3f1a59e4865d7fb9b385bfefd
marlisapee <mpee@tripadvisor.com>
2024-09-17 22:27:47 -0400

    layout changes

--- src/components/common/card/CustomCard.jsx
+++ src/components/common/card/CustomCard.jsx
@@ -1,41 +1,56 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
+import { GiBookPile } from 'react-icons/gi';
 import NullBook from '../book/NullBook';
 import { useNavigate } from 'react-router-dom';
+import useDeviceType from '../../../hooks/useDeviceType';
+import './CustomCard.css';
+import { Colors } from '../../../config';
 
 const CustomCard = ({ book }) => {
   const navigate = useNavigate();
+  const deviceType = useDeviceType();
+  const showBiggerCard = deviceType === 'mobile' || deviceType === 'tablet';
 
   const handleNavigate = () => {
     navigate(`/book/${book.title}/${book.id}`);
   };
 
   const shortenedDescription = (description) => {
-    if (description.length > 60) return description.substring(0, 60) + '...';
+    if (description.length > 60) return description.substring(0, 70) + '...';
     return description;
   };
-  return (
-    <Card bg="light" border="dark" className="mb-5" style={{ width: '18rem' }}>
-      {book.imgUrl ? (
-        <Card.Img variant="top" src={book.imgUrl} onClick={handleNavigate} />
-      ) : (
-        <NullBook />
-      )}
 
-      <Card.Body>
+  return (
+    <Card bg="light" border="dark" className="mb-5 custom-card">
+      <div className="custom-card-img-container">
+        {book.imgUrl ? (
+          <Card.Img
+            variant="top"
+            src={book.imgUrl}
+            onClick={handleNavigate}
+            className="custom-card-img"
+          />
+        ) : (
+          <NullBook />
+        )}
+      </div>
+      <Card.Body className="d-flex flex-column">
         <Card.Title>{book.title}</Card.Title>
-        <Card.Text>{shortenedDescription(book.description)}</Card.Text>
+        <Card.Text className="flex-grow-1">
+          {shortenedDescription(book.description)}
+        </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush bg-body-tertiary">
         {book.authors.map((author) => (
-          <ListGroup.Item bg key={book.id + author.name}>
+          <ListGroup.Item key={book.id + author.name}>
             {author.name}
           </ListGroup.Item>
         ))}
       </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Details</Card.Link>
-        <Card.Link href="#">Add to TBR</Card.Link>
+      <Card.Body className="text-center">
+        <Card.Text>Add to TBR</Card.Text>
+        <GiBookPile fontSize="30pt" color={Colors.forestGreen} />
       </Card.Body>
     </Card>
   );
